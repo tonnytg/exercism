3cd1e466a305e47dc0699542e53f32e8c4ecab83
Antonio Thomacelli <tonnytg@gmail.com>
2021-08-16 21:45:43 -0300

    feat: add solution for hamming

---
+++ hamming/go.mod
@@ -0,0 +1,3 @@
+module github.com/tonnytg/exercism/hamming
+
+go 1.16
---
+++ hamming/hamming.go
@@ -0,0 +1,26 @@
+package hamming
+
+import (
+	"errors"
+)
+// Distance check dna length and compare differences between them
+func Distance(a, b string) (int, error) {
+	switch {
+	case a == b:
+		// a and b equal
+		return 0, nil
+	case len(a) != len(b):
+		// cant compare differences dna
+		return 0, errors.New("length of dns is different")
+	default:
+		// compare letter by letter and count differences
+		diff := 0
+		for i := 0; i < len(a); i++ {
+			if a[i] != b[i] {
+				diff++
+			}
+		}
+		// return the hammig diferences
+		return diff, nil
+	}
+}
