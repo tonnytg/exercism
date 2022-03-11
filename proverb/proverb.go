9791a496be35a9f9498be195b436e70ed1b1c292
Antonio Thomacelli <tonnytg@gmail.com>
2022-03-10 21:59:48 -0300

    fix identation

--- proverb/proverb.go
+++ proverb/proverb.go
@@ -5,16 +5,16 @@ import "fmt"
 // Proverb return a empty slice of string or many phrases.
 func Proverb(rhyme []string) (a []string) {
 
-    // if slice of string had more than 1 word
-    for i:=1; i < len(rhyme); i++{
-        // rhyme[i-1] -> get penultimate word
-        // rhyme[i] -> get last word
-        a = append(a, fmt.Sprintf("For want of a %s the %s was lost.", rhyme[i-1], rhyme[i]))
-    }
+	// if slice of string had more than 1 word
+	for i := 1; i < len(rhyme); i++ {
+		// rhyme[i-1] -> get penultimate word
+		// rhyme[i] -> get last word
+		a = append(a, fmt.Sprintf("For want of a %s the %s was lost.", rhyme[i-1], rhyme[i]))
+	}
 	// get last word of slice
 	if len(rhyme) > 0 {
-        a = append(a, fmt.Sprintf("And all for the want of a %s.", rhyme[0]))
-    }
+		a = append(a, fmt.Sprintf("And all for the want of a %s.", rhyme[0]))
+	}
 	// return all
-    return a
+	return a
 }
